effc9fb97ba32cae8c38e60fc2387df0dff993e8
Joskar Hernandez <joskarandres97@gmail.com>
2024-08-20 20:06:52 -0400

    Feature: swagger implementation - Added documentation for Create user operation - Added documentation for Error handling

--- src/main/java/com/joskar/smartjob/test/controllers/UserREST.java
+++ src/main/java/com/joskar/smartjob/test/controllers/UserREST.java
@@ -3,6 +3,9 @@ package com.joskar.smartjob.test.controllers;
 import com.joskar.smartjob.test.controllers.dto.CreateUserRequestDTO;
 import com.joskar.smartjob.test.controllers.dto.CreateUserResponseDTO;
 import com.joskar.smartjob.test.services.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +20,13 @@ public class UserREST {
     @Autowired
     private UserService userService;
 
+    @Operation(
+            summary = "API para crear un usuario y asignarle telefonos"
+    )
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Usuario creado satisfactoramente"),
+            @ApiResponse(responseCode = "400", description = "Error de validacion en el DTO o duplicidad de datos")
+    })
     @PostMapping
     public ResponseEntity<CreateUserResponseDTO> createUser(@RequestBody CreateUserRequestDTO createUserRequestDTO) {
         return ResponseEntity.ok(userService.createUser(createUserRequestDTO));
--- src/main/java/com/joskar/smartjob/test/validators/advices/GlobalControllerAdvice.java
+++ src/main/java/com/joskar/smartjob/test/validators/advices/GlobalControllerAdvice.java
@@ -8,12 +8,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import java.util.Map;
 
 @ControllerAdvice
 public class GlobalControllerAdvice {
     @ExceptionHandler(InvalidParameterConstraintException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
     public ResponseEntity<CustomErrorDto> handleInvalidParametersConstraintException(InvalidParameterConstraintException ex) {
         CustomErrorDto customErrorDto = new CustomErrorDto();
         customErrorDto.setErrors(ex.getErrors());
